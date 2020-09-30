ebebbc1a21a211cc77d4fcfeab5d484b84e4c8e5
Zack Hall <zackhall@outlook.com>
2020-09-29 20:37:09 -0400

    Add padding to FilmStrip

--- src/components/FilmStrip.tsx
+++ src/components/FilmStrip.tsx
@@ -10,11 +10,14 @@ export interface FilmStripProps {
 const FilmStrip: React.FC<FilmStripProps> = ({ photos }) => {
   return (
     <div className='w-full h-48'>
-      {photos.map((photo) => (
+      {photos.map((photo, i) => (
         <img
+          key={photo.src}
           src={photo.src}
           alt={photo.alt}
-          className='inline-block w-4/12 h-full object-cover'
+          className={`inline-block w-4/12 h-full object-cover ${
+            i !== 0 ? 'pl-2' : ''
+          } ${i < photos.length - 1 ? 'pr-2' : ''}`}
         />
       ))}
     </div>
