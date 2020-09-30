c6b3c16631779965a8b0f588c86006754cc4b004
Zack Hall <zackhall@outlook.com>
2020-09-30 18:18:26 -0400

    Add link to filmstrip, fix spacing.

--- src/components/FilmStrip.tsx
+++ src/components/FilmStrip.tsx
@@ -1,25 +1,38 @@
 import React from 'react'
 
+import Anchor from '@components/Anchor'
+
 export interface FilmStripProps {
   photos: Array<{
     src: any
     alt: string
   }>
+  link?: {
+    text: string
+    path: string
+  }
 }
 
-const FilmStrip: React.FC<FilmStripProps> = ({ photos }) => {
+const FilmStrip: React.FC<FilmStripProps> = ({ photos, link }) => {
   return (
-    <div className='w-full h-48'>
-      {photos.map((photo, i) => (
-        <img
-          key={photo.src}
-          src={photo.src}
-          alt={photo.alt}
-          className={`inline-block w-4/12 h-full object-cover ${
-            i !== 0 ? 'pl-2' : ''
-          } ${i < photos.length - 1 ? 'pr-2' : ''}`}
-        />
-      ))}
+    <div className='my-12 lg:my-16'>
+      <div className='w-full h-48'>
+        {photos.map((photo, i) => (
+          <img
+            key={photo.src}
+            src={photo.src}
+            alt={photo.alt}
+            className={`inline-block w-4/12 h-full object-cover ${
+              i !== 0 ? 'pl-2' : ''
+            } ${i < photos.length - 1 ? 'pr-2' : ''}`}
+          />
+        ))}
+      </div>
+      {link && link.text && link.path ? (
+        <div className='text-right mt-8'>
+          <Anchor to={`/${link.path}`}>{link.text}</Anchor>
+        </div>
+      ) : null}
     </div>
   )
 }
