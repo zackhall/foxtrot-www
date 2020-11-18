6a81b3eeae14cc8f625920277b5a994426ee2502
Zack Hall <zackhall@outlook.com>
2020-11-17 21:43:12 -0500

    Remove unused deps

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,15 +1,10 @@
 import CMS from 'netlify-cms-app'
-import uploadcare from 'netlify-cms-media-library-uploadcare'
-import cloudinary from 'netlify-cms-media-library-cloudinary'
 import { MdxControl, MdxPreview } from 'netlify-cms-widget-mdx'
 
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 import ServicePagePreview from './preview-templates/ServicePagePreview'
 
 CMS.registerWidget('mdx', MdxControl, MdxPreview)
-
-CMS.registerMediaLibrary(uploadcare)
-CMS.registerMediaLibrary(cloudinary)
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('services', ServicePagePreview)
