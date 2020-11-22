0388506d3a5b16ba685db94da1766b2d5db567c6
Zack Hall <zackhall@outlook.com>
2020-11-22 18:34:25 -0500

    Fix deps for netlify cms

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,13 +1,10 @@
 import CMS from 'netlify-cms-app'
-import { MdxControl, MdxPreview } from 'netlify-cms-widget-mdx'
 
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 import ServicePagePreview from './preview-templates/ServicePagePreview'
 import ContactPagePreview from './preview-templates/ContactPagePreview'
 import DefaultPagePreview from './preview-templates/DefaultPagePreview'
 import GalleryPagePreview from './preview-templates/GalleryPagePreview'
-
-CMS.registerWidget('mdx', MdxControl, MdxPreview)
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('services', ServicePagePreview)
